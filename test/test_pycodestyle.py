3e908cdd35d6152821ae018d8291baf27ff9c5c6
Darkdragon-001 <darkdragon-001@web.de>
2017-11-29 03:42:10 +0100

    use pycodestyle instead of pep8

--- test/test_pycodestyle.py
+++ test/test_pycodestyle.py
@@ -1,15 +1,15 @@
 
 import os
 import unittest
-import pep8
+import pycodestyle
 
 
-class PackagePep8TestCase(unittest.TestCase):
+class PackagePycodestyleTestCase(unittest.TestCase):
 
-    def test_pep8(self):
+    def test_pycodestyle(self):
         basepath = os.path.abspath(
             os.path.join(os.path.dirname(__file__), ".."))
-        style = pep8.StyleGuide(quiet=True)
+        style = pycodestyle.StyleGuide(quiet=True)
         res = style.check_files(paths=[basepath]).total_errors
         self.assertEqual(res, 0)
 
